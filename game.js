59a05eb18c533470a2e98661e1b581c0460b5c59
Nicholas Semaan <38543748+NichSaymaan@users.noreply.github.com>
2018-05-09 20:54:44 -0700

    initial commit

--- game.js
+++ game.js
@@ -1,11 +1,92 @@
 // functions
 
-function createGameState(){}
+function createGameState(){
+    var game = {};
+
+    game.score = 0
+
+    game.isDead = false;
+    game.playerX = 2;
+    game.playerY = 2;
+
+    game.coinX = 0;
+    game.coinY = 0;
+
+    var grid = [];
+    grid.push([1,1,1,1,1]);
+    grid.push([1,0,0,0,1]);
+    grid.push([1,0,0,0,1]);
+    grid.push([0,0,0,0,0]);
+    grid.push([1,0,0,0,1]);
+    grid.push([1,0,0,0,1]);
+    grid.push([1,1,1,1,1]);
+
+    game.grid = grid;
+
+    game.rows = grid.length;
+    game.cols = grid[0].length;
+
+    setRandomCoin(game);
+
+    return game;
+
+}
 function createEmptyGrid(){}
 function createLevel(){}
-function setRandomCoin(){}
-function drawGameState(){}
-function updateGameState(){}
+function setRandomCoin(game){
+
+    var collided = true;
+
+    while(collided){
+        game.coinX = Math.floor(random(game.cols - 1));
+        game.coinY = Math.floor(random(game.rows - 1));
+
+        // check if coin hit the wall
+        var x = game.coinX;
+        var y = game.coinY;
+        var hitWall = (game.grid[y][x] == 1);
+
+        var hitPlayer = (game.coinX == game.playerX && game.coinY == game.playerY);
+        collided = hitWall || hitPlayer;
+    }
+}
+
+function hitCoin(game) {
+    return (game.coinX == game.playerX && game.coinY == game.playerY);
+}
+
+function drawGameState(game){
+    // draw the walls
+    for(var c = 0; c < game.cols; c++){
+        for(var r = 0; r < game.rows; r++){
+            drawPoint(c, r, game.grid[r][c]);
+        }    
+    }
+    
+    // draw the player
+    drawPoint(game.playerX, game.playerY,2);
+
+    // draw the coin
+    drawPoint(game.coinX, game.coinY,3);
+
+    // draw the score
+    fill("red");
+    text('Score: ' + game.score, 10, 10);
+}
+
+function updateGameState(game){
+    // check if player hit wall
+    var x = game.playerX;
+    var y = game.playerY;
+    if (game.grid[y][x] == 1) game.isDead = true;
+
+    // check if player hit coin
+    if (hitCoin(game)){
+        game.score += 100;
+        setRandomCoin(game);
+    }
+}
+
 function addNextCoin(){}
 function handleKeyboard(){}
 function checkPlayerPosition(){}
